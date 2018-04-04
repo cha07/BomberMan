bbe5d678ca3d4de748533125a7eafc172609c86d
3p3h97 <pierre.hermey@laposte.net>
2018-04-04 16:55:28 +0200

    new images

--- scripts/hero.js
+++ scripts/hero.js
@@ -20,9 +20,9 @@ function afficherMap() {
     var floor;
     var hero = document.querySelector('#hero');
 
-    for (x = 0; x < 14; x++) {
+    for (x = 0; x < 17; x++) {
 
-        for (y = 0; y < 10; y++) {
+        for (y = 0; y < 11; y++) {
 
             if (grille[y][x] === 2) {
                 wall = document.createElement("div");
@@ -49,7 +49,7 @@ function afficherMap() {
                 map.appendChild(floor);
                 floor.style.top = y * 50 + "px";
                 floor.style.left = x * 50 + "px";
-                floor.style.backgroundImage = "url('../medias/grass.svg')";
+
             }
         }
     }
@@ -73,30 +73,30 @@ document.addEventListener("keydown", function (e) {
 
                 if (grille[posBlockTop - 1][posBlockLeft] == 1) {
                     hero.style.top = (posBlockTop - 1) * 50 + "px";
-                    hero.style.backgroundImage = "url('img/h-top.png')";
                 }
+                hero.style.backgroundImage = "url('img/h-top.png')";
                 break;
 
             case 39:
                 if (grille[posBlockTop][posBlockLeft + 1] == 1) {
                     hero.style.left = (posBlockLeft + 1) * 50 + "px";
-                    hero.style.backgroundImage = "url('img/h-d.png')";
                 }
+                hero.style.backgroundImage = "url('img/h-d.png')";
                 break;
 
             case 40:
 
                 if (grille[posBlockTop + 1][posBlockLeft] == 1) {
                     hero.style.top = (posBlockTop + 1) * 50 + "px";
-                    hero.style.backgroundImage = "url('img/h.png')";
                 }
+                hero.style.backgroundImage = "url('img/h.png')";
                 break;
 
             case 37:
                 if (grille[posBlockTop][posBlockLeft - 1] == 1) {
-                    hero.style.left = (posBlockLeft - 1) * 50 + "px";
-                    hero.style.backgroundImage = "url('img/h-g.png')";
+                    hero.style.left = (posBlockLeft - 1) * 50 + "px";  
                 }
+                hero.style.backgroundImage = "url('img/h-g.png')";
                 break;
 
             case 32:
@@ -116,52 +116,4 @@ document.addEventListener("keydown", function (e) {
                 break;
         }
     }
-});
-
-// var cadre = document.querySelector('#map');
-// // variables objet v
-// var hero = document.querySelector('#hero'); 
-// var x; var y ; 
-// var move = 50;
-// // programmation des touches de direction 
-// document.onkeydown = function(event) { 
-// if (event.keyCode == 37) gauche(hero.offsetLeft); 
-// if (event.keyCode == 39) droite(hero.offsetLeft); 
-// if (event.keyCode == 38) haut(hero.offsetTop); 
-// if (event.keyCode == 40) bas(hero.offsetTop); 
-// } // fin fonction
-
-// // quatre fonctions de déplacement 
-// function gauche(left) { 
-
-//     if (grilleleft > 10){
-//     return  hero.style.left = left - move + "px";
-//     }
-// return hero.style.left = "0px";
-// } 
-
-// function droite(left) {
-
-//     if(left > (cadre.offsetWidth - move))
-//         return hero.style.left = cadre.offsetWidth + "px";
-
-//     if((left + hero.offsetWidth) < cadre.offsetWidth)
-//         return hero.style.left = left + move + "px";
-// }
-
-// function haut(top) { 
-
-//     if (top > 10){
-//     return  hero.style.top = top - move + "px";
-//     }
-// return hero.style.top = "0px";
-// } 
-
-// function bas(top) {
-
-//     if(top > (cadre.offsetHeight - move))
-//         return hero.style.top = cadre.offsetHeight + "px";
-
-//     if((top + hero.offsetHeight) < cadre.offsetHeight)
-//         return hero.style.top = top + move + "px";
-// } 
+})
