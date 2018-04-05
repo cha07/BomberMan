a73ac86919eac7a2f490e24f30f25d418fa7ba4a
maximejanod <maxime.j@codeur.online>
2018-04-05 10:00:13 +0200

    explo

--- scripts/hero.js
+++ scripts/hero.js
@@ -1,16 +1,17 @@
 var grille = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-];
+    [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+    [4, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4],
+    [4, 1, 2, 1, 2, 3, 2, 3, 2, 3, 2, 3, 2, 1, 2, 3, 2, 3, 4],
+    [4, 1, 1, 3, 1, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4],
+    [4, 1, 2, 3, 2, 3, 2, 3, 2, 1, 2, 3, 2, 1, 2, 1, 2, 3, 4],
+    [4, 1, 1, 3, 1, 3, 1, 3, 3, 3, 3, 3, 1, 1, 1, 3, 3, 3, 4],
+    [4, 3, 2, 3, 2, 1, 2, 3, 2, 1, 2, 1, 2, 1, 2, 1, 2, 3, 4],
+    [4, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 4],
+    [4, 3, 2, 1, 2, 1, 2, 3, 2, 1, 2, 3, 2, 1, 2, 1, 2, 1, 4],
+    [4, 3, 1, 1, 1, 1, 1, 3, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 4],
+    [4, 3, 2, 1, 2, 1, 2, 3, 2, 3, 2, 3, 2, 1, 2, 1, 2, 1, 4],
+    [4, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4],
+    [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]];
 
 function afficherMap() {
 
@@ -18,11 +19,20 @@ function afficherMap() {
     var wall;
     var wallDestruct;
     var floor;
-    var hero = document.querySelector('#hero');
+    var bord;
 
-    for (x = 0; x < 14; x++) {
+    for (x = 0; x < 19; x++) {
 
-        for (y = 0; y < 10; y++) {
+        for (y = 0; y < 13; y++) {
+
+            if (grille[y][x] === 4) {
+                wall = document.createElement("div");
+                wall.setAttribute("class", "bord");
+                map.appendChild(wall);
+                wall.style.top = y * 50 + "px";
+                wall.style.left = x * 50 + "px";
+                wall.style.backgroundImage = "url('img/bord.png')";
+            }
 
             if (grille[y][x] === 2) {
                 wall = document.createElement("div");
@@ -40,7 +50,7 @@ function afficherMap() {
                 map.appendChild(wallDestruct);
                 wallDestruct.style.top = y * 50 + "px";
                 wallDestruct.style.left = x * 50 + "px";
-                wallDestruct.style.backgroundImage = "url('../medias/rocher.svg')";
+                wallDestruct.style.backgroundImage = "url('img/wall-destruct.png')";
 
             } else if (grille[y][x] === 1) {
 
@@ -49,7 +59,7 @@ function afficherMap() {
                 map.appendChild(floor);
                 floor.style.top = y * 50 + "px";
                 floor.style.left = x * 50 + "px";
-                floor.style.backgroundImage = "url('../medias/grass.svg')";
+                floor.style.backgroundImage = "url('img/floor.png')";
             }
         }
     }
@@ -57,7 +67,8 @@ function afficherMap() {
 
 afficherMap();
 
-
+var hero = document.querySelector('#hero');
+var bomb = document.querySelector("#bomb");
 var controlActive = true
 document.addEventListener("keydown", function (e) {
 
@@ -73,44 +84,41 @@ document.addEventListener("keydown", function (e) {
 
                 if (grille[posBlockTop - 1][posBlockLeft] == 1) {
                     hero.style.top = (posBlockTop - 1) * 50 + "px";
-                    hero.style.backgroundImage = "url('img/h-top.png')";
                 }
+                hero.style.backgroundImage = "url('img/h-top.png')";
                 break;
 
             case 39:
                 if (grille[posBlockTop][posBlockLeft + 1] == 1) {
                     hero.style.left = (posBlockLeft + 1) * 50 + "px";
-                    hero.style.backgroundImage = "url('img/h-d.png')";
                 }
+                hero.style.backgroundImage = "url('img/h-d.png')";
                 break;
 
             case 40:
 
                 if (grille[posBlockTop + 1][posBlockLeft] == 1) {
                     hero.style.top = (posBlockTop + 1) * 50 + "px";
-                    hero.style.backgroundImage = "url('img/h.png')";
                 }
+                hero.style.backgroundImage = "url('img/h.png')";
                 break;
 
             case 37:
                 if (grille[posBlockTop][posBlockLeft - 1] == 1) {
                     hero.style.left = (posBlockLeft - 1) * 50 + "px";
-                    hero.style.backgroundImage = "url('img/h-g.png')";
                 }
+                hero.style.backgroundImage = "url('img/h-g.png')";
                 break;
 
             case 32:
-                if (posBomberman === 0) {
-                    bomb.style.left = bombermanLeft + "px";
-                    bomb.style.top = bombermanTop + "px";
+                if (posHero === 0) {
+                    bomb.style.left = heroLeft + "px";
+                    bomb.style.top = heroTop + "px";
                     bomb.style.backgroundImage = "url('img/bomb.png')";
                     bomb.style.display = "block";
 
-                    var audio = new Audio('../medias/bombdrop.wav');
-                    audio.play();
-
-                    setTimeout(exploseTheBomb, 3000);
-                    setTimeout(suppexplosion, 4000);
+                    setTimeout(boom, 2000);
+                    setTimeout(suppexplosion, 1000);
 
                 }
                 break;
@@ -118,50 +126,163 @@ document.addEventListener("keydown", function (e) {
     }
 });
 
-// var cadre = document.querySelector('#map');
-// // variables objet v
-// var hero = document.querySelector('#hero'); 
-// var x; var y ; 
-// var move = 50;
-// // programmation des touches de direction 
-// document.onkeydown = function(event) { 
-// if (event.keyCode == 37) gauche(hero.offsetLeft); 
-// if (event.keyCode == 39) droite(hero.offsetLeft); 
-// if (event.keyCode == 38) haut(hero.offsetTop); 
-// if (event.keyCode == 40) bas(hero.offsetTop); 
-// } // fin fonction
+// bombe qui explose pas x)
 
-// // quatre fonctions de déplacement 
-// function gauche(left) { 
+function suppexplosion() {
 
-//     if (grilleleft > 10){
-//     return  hero.style.left = left - move + "px";
-//     }
-// return hero.style.left = "0px";
-// } 
+    bomb.style.display = "none";
+    var element = document.getElementsByClassName('explosion');
 
-// function droite(left) {
+    for (var i = element.length - 1; i >= 0; i--) {
+        element[i].parentElement.removeChild(element[i]);
+    }
 
-//     if(left > (cadre.offsetWidth - move))
-//         return hero.style.left = cadre.offsetWidth + "px";
+}
 
-//     if((left + hero.offsetWidth) < cadre.offsetWidth)
-//         return hero.style.left = left + move + "px";
-// }
+function boom(){
+    var posBombLeft = bomb.offsetLeft / 50;
+    var posBombTop = bomb.offsetTop / 50;
+    var explosion;
+    
+    if (grille[posBombTop + 1][posBombLeft] != 4 ) {
+    
+    explosion = document.createElement("div");
+    explosion.setAttribute("class", "explosion");
+    map.appendChild(explosion);
+    explosion.style.top = (posBombTop + 1) * 50 + "px";
+    explosion.style.left = posBombLeft * 50 + "px";
+    bomb.style.backgroundImage = "url('img/bombex.png')";
+    explosion.style.backgroundImage = "url('img/bombex.png')";
+    explosion.style.backgroundSize = "50px";
+    
+    } if (grille[posBombTop - 1][posBombLeft] = 3 ) {
+    
+    explosion = document.createElement("div");
+    explosion.setAttribute("class", "explosion");
+    map.appendChild(explosion);
+    explosion.style.top = (posBombTop - 1) * 50 + "px";
+    explosion.style.left = posBombLeft * 50 + "px";
+    bomb.style.backgroundImage = "url('img/bombex.png')";
+    explosion.style.backgroundImage = "url('img/bombex.png')";
+    explosion.style.backgroundSize = "50px";
+    
+    } if (grille[posBombTop][posBombLeft + 1] = 3 ) {
+    
+    explosion = document.createElement("div");
+    explosion.setAttribute("class", "explosion");
+    map.appendChild(explosion);
+    explosion.style.top = posBombTop * 50 + "px";
+    explosion.style.left = (posBombLeft + 1) * 50 + "px";
+    bomb.style.backgroundImage = "url('img/bombex.png')";
+    explosion.style.backgroundImage = "url('img/bombex.png')";
+    explosion.style.backgroundSize = "50px";
+    
+    
+    
+    } if (grille[posBombTop][posBombLeft - 1] = 3 ) {
+    
+    explosion = document.createElement("div");
+    explosion.setAttribute("class", "explosion");
+    map.appendChild(explosion);
+    explosion.style.top = posBombTop * 50 + "px";
+    explosion.style.left = (posBombLeft - 1) * 50 + "px";
+    bomb.style.backgroundImage = "url('img/bombex.png')";
+    explosion.style.backgroundImage = "url('img/bombex.png')";
+    explosion.style.backgroundSize = "50px";
+    
+    }
+    }
 
-// function haut(top) { 
+    // function exploseTheBomb(wallDestruct) {
 
-//     if (top > 10){
-//     return  hero.style.top = top - move + "px";
-//     }
-// return hero.style.top = "0px";
-// } 
+    //     var posBombLeft = bomb.offsetLeft / 50;
+    //     var posBombTop = bomb.offsetTop / 50;
+    //     var x, y = 0;
+    //     var explosion;
+    
+    //     var posMonsterLeft = monster.offsetLeft / 50;
+    //     var posMonsterTop = monster.offsetTop / 50;
+    
+    //     audio2.play();
+    
+    //     if (grille[posBombTop - 1][posBombLeft] == 3 || grille[posBombTop - 1][posBombLeft] == 1) {
+    
+    //         explosion = document.createElement("div");
+    //         explosion.setAttribute("class", "explosion");
+    //         map.appendChild(explosion);
+    //         explosion.style.top = (posBombTop - 1) * 50 + "px";
+    //         explosion.style.left = posBombLeft * 50 + "px";
+    //         bomb.style.backgroundImage = "url('img/bomb.png')";
+    //         explosion.style.backgroundImage = "url('img/carre-3.png')";
+    
+    //         breakMonster(explosion);
+    //         breakBlock(explosion);
+    //     }
+    
+    //     if (grille[posBombTop + 1][posBombLeft] == 3 || grille[posBombTop + 1][posBombLeft] == 1) {
+    
+    //         explosion = document.createElement("div");
+    //         explosion.setAttribute("class", "explosion");
+    //         map.appendChild(explosion);
+    //         explosion.style.top = (posBombTop + 1) * 50 + "px";
+    //         explosion.style.left = posBombLeft * 50 + "px";
+    //         bomb.style.backgroundImage = "url('img/bomb.png')";
+    //         explosion.style.backgroundImage = "url('img/carre-3.png')";
+    //         breakMonster(explosion);
+    //         breakBlock(explosion);
+    //     }
+    
+    //     if (grille[posBombTop][posBombLeft + 1] == 3 || grille[posBombTop][posBombLeft + 1] == 1) {
+    
+    //         explosion = document.createElement("div");
+    //         explosion.setAttribute("class", "explosion");
+    //         map.appendChild(explosion);
+    //         explosion.style.top = posBombTop * 50 + "px";
+    //         explosion.style.left = (posBombLeft + 1) * 50 + "px";
+    //         bomb.style.backgroundImage = "url('img/bomb.png')";
+    //         explosion.style.backgroundImage = "url('img/carre-3.png')";
+    //         breakMonster(explosion);
+    //         breakBlock(explosion);
+    //     }
+    
+    //     if (grille[posBombTop][posBombLeft - 1] == 3 || grille[posBombTop][posBombLeft - 1] == 1) {
+    
+    //         explosion = document.createElement("div");
+    //         explosion.setAttribute("class", "explosion");
+    //         map.appendChild(explosion);
+    //         explosion.style.top = posBombTop * 50 + "px";
+    //         explosion.style.left = (posBombLeft - 1) * 50 + "px";
+    //         bomb.style.backgroundImage = "url('img/bomb.png')";
+    //         explosion.style.backgroundImage = "url('img/carre-3.png')";
+    //         breakMonster(explosion);
+    //         breakBlock(explosion);
+    
+    //     } else {
+    //         bomb.style.backgroundImage = "url('img/bomb.png')";
+    
+    //     }
+    // }
 
-// function bas(top) {
+    // function breakBlock(explosion) {
 
-//     if(top > (cadre.offsetHeight - move))
-//         return hero.style.top = cadre.offsetHeight + "px";
-
-//     if((top + hero.offsetHeight) < cadre.offsetHeight)
-//         return hero.style.top = top + move + "px";
-// } 
+    //     var element = document.getElementsByClassName('wall-destruct');
+    //     var wallDestructLeft = 0;
+    //     var wallDestructTop = 0;
+    
+    //     for (var i = element.length - 1; i >= 0; i--) {
+    
+    //         wallDestructLeft = element[i].offsetLeft / 50;
+    //         wallDestructTop = element[i].offsetTop / 50;
+    
+    //         if ((explosion.offsetTop / 50 == wallDestructTop) && (explosion.offsetLeft / 50 == wallDestructLeft)) {
+    
+    //             element[i].style.backgroundImage = "url('../medias/grass.svg')";
+    
+    //             if (grille[wallDestructTop][wallDestructLeft] == 3) {
+    //                 grille[wallDestructTop][wallDestructLeft] = 1;
+    
+    //             }
+    
+    //         }
+    //     }
+    // }
